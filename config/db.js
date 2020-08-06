aae166dbcedff8adddbd3d53fd7f60ee03d78e95
flamerged <mersad.ajanovic@googlemail.com>
2020-08-06 21:39:11 +0200

    added api routes, created user schema, added create user post route
    user creation has some basic level of validation and password hashing using brcrypt

--- config/db.js
+++ config/db.js
@@ -1,16 +1,19 @@
 const mongoose = require("mongoose");
-const config =  require("config");
+const config = require("config");
 const db = config.get("mongoURI");
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(db,{useNewUrlParser:true,useUnifiedTopology:true});
+        await mongoose.connect(db, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useCreateIndex: true,
+        });
         console.log("MongoDB connected.");
-    } catch(err) {
+    } catch (err) {
         console.error(err.message);
         process.exit(1);
     }
-}
+};
 
-module.exports = connectDB
-
+module.exports = connectDB;
