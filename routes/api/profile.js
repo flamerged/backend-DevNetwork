c42c5fa95f7da892fd2405750491c343da4f79d3
flamerged <mersad.ajanovic@googlemail.com>
2020-08-11 17:33:03 +0200

    added routes to detele experience and add+delete education

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -3,8 +3,8 @@ const router = express.Router();
 const auth = require("../../middleware/auth");
 const Profile = require("../../models/Profile");
 const User = require("../../models/User");
-const { check, validationResult } = require("express-validator");
 const { create, populate } = require("../../models/User");
+const { check, validationResult } = require("express-validator");
 
 // @route   GET /api/profile/me
 // @desc    Get current users profile
@@ -12,9 +12,7 @@ const { create, populate } = require("../../models/User");
 router.get("/me", auth, async (req, res) => {
     try {
         const profile = await Profile.findOne({ user: req.user.id }).populate(
-            "user",
-            "name",
-            "avatar"
+            "user"[("name", "avatar")]
         );
 
         if (!profile) {
@@ -171,7 +169,7 @@ router.delete("/", auth, async (req, res) => {
 });
 
 // @route   PUT /api/profile/experience
-// @desc    Add Profile experience
+// @desc    Add experience to profile
 // @access  Private
 
 router.put(
@@ -219,5 +217,112 @@ router.put(
         }
     }
 );
+
+// @route   DELETE /api/profile/experience/:exp_id
+// @desc    Delete experience from profile
+// @access  Private
+
+router.delete("/experience/:exp_id", auth, async (req, res) => {
+    try {
+        const profile = await Profile.findOne({ user: req.user.id });
+
+        // create new array with the ids of each experience
+        // find the index of the id we want to delete
+
+        const indexPosition = profile.experience
+            .map((exp) => exp.id)
+            .indexOf(req.params.exp_id);
+
+        // delete the experience by using the index to find it's position in the array
+
+        profile.experience.splice(indexPosition, 1);
+
+        await profile.save();
+        res.json(profile);
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send("Server Error");
+    }
+});
+
+// @TODO Add Route to update individual experience
+
+// @route   PUT /api/profile/education
+// @desc    Add education to profile
+// @access  Private
+
+router.put(
+    "/education",
+    [
+        auth,
+        check("school", "School is required").not().isEmpty(),
+        check("degree", "Degree is required").not().isEmpty(),
+        check("fieldofstudy", "Field of study is required").not().isEmpty(),
+        check("from", "From date is required").not().isEmpty(),
+    ],
+    async (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+
+        const {
+            school,
+            degree,
+            fieldofstudy,
+            from,
+            to,
+            current,
+            description,
+        } = req.body;
+
+        const newEd = {
+            school,
+            degree,
+            fieldofstudy,
+            from,
+            to,
+            current,
+            description,
+        };
+
+        try {
+            const profile = await Profile.findOne({ user: req.user.id });
+            profile.education.unshift(newEd);
+            await profile.save();
+            res.json(profile);
+        } catch (error) {
+            console.error(error.message);
+            res.status(500).send("Server Error");
+        }
+    }
+);
+
+// @route   DELETE /api/profile/education/:ed_id
+// @desc    Delete education from profile
+// @access  Private
+
+router.delete("/education/:ed_id", auth, async (req, res) => {
+    try {
+        const profile = await Profile.findOne({ user: req.user.id });
+
+        // create new array with the ids of each experience
+        // find the index of the id we want to delete
+
+        const indexPosition = profile.education
+            .map((ed) => ed.id)
+            .indexOf(req.params.ed_id);
+
+        // delete the experience by using the index to find it's position in the array
+
+        profile.education.splice(indexPosition, 1);
+
+        await profile.save();
+        res.json(profile);
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send("Server Error");
+    }
+});
 
 module.exports = router;
