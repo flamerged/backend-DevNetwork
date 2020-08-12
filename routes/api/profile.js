d0874d9675b1b7aac5ff4210a3f4b49dbac98669
flamerged <mersad.ajanovic@googlemail.com>
2020-08-12 23:42:23 +0200

    created routes to get all posts, get and delete post by id

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -158,7 +158,6 @@ router.get("/user/:user_id", async (req, res) => {
 
 router.delete("/", auth, async (req, res) => {
     try {
-        //@todo remove user profiles
         // Remove profile
         await Profile.findOneAndRemove({ user: req.user.id });
         //remove user
